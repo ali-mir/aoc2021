a64744f962f9388d70017b279bcd13595d744e65
Ali Mir <ali.mir@mongodb.com>
2021-12-07 10:05:16 -0500

    fix formatting

---
+++ day3/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	r "aoc2021/reader"
+	"fmt"
+)
+
+
+func main() {
+	input := r.Read()
+}
--- reader/reader.go
+++ reader/reader.go
@@ -1,43 +1,42 @@
 package reader
 
 import (
-    "fmt"
-    "os"
 	"bufio"
+	"fmt"
+	"os"
 	"strconv"
-	)
+)
 
 // Reads input file and outputs a slice of strings
 // where each element is a line in the input.
-func Read() ([]string){
-    fmt.Println("Reading file input.txt...")
-    file, err := os.Open("input.txt")
-    if err != nil {
-        panic("Opening input.txt failed")
-    }
-    defer file.Close()
-    var input []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        input = append(input, scanner.Text())
-    }
-    if (scanner.Err() != nil) {
-        panic("Reading file failed")
-    }
-    fmt.Println("Size of input:", len(input))
-    return input
+func Read() []string {
+	fmt.Println("Reading file input.txt...")
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic("Opening input.txt failed")
+	}
+	defer file.Close()
+	var input []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+	if scanner.Err() != nil {
+		panic("Reading file failed")
+	}
+	fmt.Println("Size of input:", len(input))
+	return input
 }
 
-
 // Helpful functions to operate on input slices
-func StringsToInts(strings []string, ) []int {
-    var ints []int
-    for _, s := range strings {
-        parsedInt, err := strconv.Atoi(s)
-        if err != nil {
-            panic("Converting string to int failed")
-        }
-        ints = append(ints, parsedInt)
-    }
-    return ints
+func StringsToInts(strings []string) []int {
+	var ints []int
+	for _, s := range strings {
+		parsedInt, err := strconv.Atoi(s)
+		if err != nil {
+			panic("Converting string to int failed")
+		}
+		ints = append(ints, parsedInt)
+	}
+	return ints
 }
